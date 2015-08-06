1c48a2a6bc1facc2181d486c36fe191fad830370
Rosario Tarabocchia <rtarabocchia@gmail.com>
2015-08-06 17:33:33 -0400

    Initial Commit

---
+++ Programming Exercise/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Programming Exercise
+//
+//  Created by Rosario Tarabocchia on 8/6/15.
+//  Copyright (c) 2015 RLDT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
